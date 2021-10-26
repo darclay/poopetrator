e07cb838e4df2166293327c1873a4b8a4fbf3180
David Gurule <davidgurulejr@gmail.com>
2021-10-26 08:14:12 -0600

    trying to render random photo

--- src/components/FeelingCheeky.jsx
+++ src/components/FeelingCheeky.jsx
@@ -8,23 +8,38 @@ const API_URL = "https://api.airtable.com/v0/appAT4ne9vTP46u1M/Table%201?api_key
 
 const FeelingCheeky = () => {
   const [ plopPosts, setPlopPosts ] = useState([]); 
+ 
   
   useEffect(() => {
     const getData = async () => {
       const response = await axios.get(`${API_URL}${API_KEY}`);
-      setPlopPosts(response.data.records);
+      const results = response.data.records;
+      console.log(results);
+      const randIndex = Math.floor(Math.random() * results.length);
+      setPlopPosts(results[randIndex]);
+      console.log(typeof plopPosts);
+    // -------------------------------------------
+      const photoDis = plopPosts.fields.PicUpload[0].thumbnails.large.url;
+    
+      if (photoDis) {
+      console.log(plopPosts.fields.PicUpload[0].thumbnails.large.url);
+      }else {
+      console.log("no pic available");
+      }
+    
+    
     }
     getData();
   }, []);
 
-  
   return (
     <div>
       <Link to="/">
         <h2>BACK TO HOME</h2>
         </Link>
       <h2>Feeling Cheeky</h2>
-      
+      {/* <img src={plopPosts.fields.PicUpload[0].thumbnails.large.url} alt="dog poop"></img> */}
+
      
       
     </div>
