7ae893be05b4f1ad4d9e753e76d95dbf76e89d76
David Gurule <davidgurulejr@gmail.com>
2021-10-28 15:33:58 -0600

    fixed errors

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
       Well, your dog's poo should be important to you.  Did you know that you can tell a lot about your dog's health from their doodoo?<br/>Scotology or Coprology is the science behind looking at a doocky and inducing not only what your animal eats but the animals overall health.
       <br/>
 
-      Click <a href="https://www.purina.co.uk/articles/dogs/health/digestion/guide-to-dog-poop" target="_blank">HERE</a> to see more information on your dogs poo and you.
+      Click <a href="https://www.purina.co.uk/articles/dogs/health/digestion/guide-to-dog-poop">HERE</a> to see more information on your dogs poo and you.
       </p>
 
       <p>https://en.wikipedia.org/wiki/Scatology</p>
