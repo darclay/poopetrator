90f8c688b2120616b9082488d2bf92e36cd4d768
David Gurule <davidgurulejr@gmail.com>
2021-10-29 06:56:17 -0600

    api tried again

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 import axios from "axios";
 import { useEffect, useState } from 'react';
 
-const API_URL = `https://api.airtable.com/v0/appAT4ne9vTP46u1M/Table%201?api_key=${process.env.REACT_APP_API_KEY}`
+// const API_URL = `https://api.airtable.com/v0/appAT4ne9vTP46u1M/Table%201?api_key=${process.env.REACT_APP_API_KEY}`
 const API_URL = 'https://api.airtable.com/v0/appAT4ne9vTP46u1M/Table%201?api_key=keyuIN18WVkKH2hMu'
 
 
