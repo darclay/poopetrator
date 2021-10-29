1640bf10d744f3bde5f4d357928920f94e51faa8
David Gurule <davidgurulejr@gmail.com>
2021-10-29 07:35:00 -0600

    api key fixed

--- src/components/Newplop.jsx
+++ src/components/Newplop.jsx
@@ -3,8 +3,8 @@ import { useState } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import "./Newplop.css";
 
-// const API_URL = `https://api.airtable.com/v0/appAT4ne9vTP46u1M/Table%201?api_key=${process.env.REACT_APP_API_KEY}`;
-const API_URL = 'https://api.airtable.com/v0/appAT4ne9vTP46u1M/Table%201?api_key=keyuIN18WVkKH2hMu'
+const API_URL = `https://api.airtable.com/v0/appAT4ne9vTP46u1M/Table%201?api_key=${process.env.REACT_APP_API_KEY}`;
+
 
 const Newplop = ({ togglePost, setTogglePost }) => {
   const [ Title, setTitle ] = useState('');
